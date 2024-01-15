269c8404c6c9647d199d0c371d0ab6b525de08aa
Iain Soars <iain.soars@bca.com>
2024-01-15 16:27:55 +0000

    Added React

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Options from './Options';
+
+import './App.css';
+
+const App = () => {
+  return (
+      <div className="app">
+        <div className="header">
+          <h1>Can't make a decision?</h1>
+        </div>
+        <Options/>
+      </div>
+  );
+}
+
+export default App;
